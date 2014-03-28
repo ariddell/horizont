9e2874c6e032d2b4f7eb7507d6c619a066c2bfe1
Allen Riddell <abr@ariddell.org>
2014-03-28 12:38:21 +0100

    TST: test fitting the AP corpus with LDA

---
+++ horizont/tests/test_lda_ap.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+import unittest
+
+from horizont import LDA, utils
+
+
+class TestLDANews(unittest.TestCase):
+
+    test_dir = os.path.dirname(__file__)
+    ap_ldac_fn = os.path.join(test_dir, 'ap.dat')
+    dtm = utils.ldac2dtm(open(ap_ldac_fn))
+
+    def test_lda_news(self):
+        dtm = self.dtm
+        n_topics = 20
+        n_iter = 100
+        random_state = 1
+        clf = LDA(n_topics=n_topics, n_iter=n_iter, random_state=random_state)
+        doctopic = clf.fit_transform(dtm)
+        self.assertEqual(len(doctopic), len(dtm))
